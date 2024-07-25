a7e780c46b608d2cdb4985a5238a20e004529faf
IvanDS-OF <ivan.durans@outlook.com>
2024-07-24 18:11:02 -0600

    Working with Api Keys

--- Use of APIS/ApisWithKEY.py
+++ Use of APIS/ApisWithKEY.py
@@ -1,3 +1,25 @@
 #There are some apis that required some Key to be used, or just for security. 
 #In this project we will review how to implement the keys when we want use an API
 
+
+#Once obtained the key and the setting steps, lets to code
+#Iport all the libraries
+import requests
+
+url = 'https://openweathermap.org/forecast5#name5'
+#url = 'https://api.openweathermap.org/data/2.5/forecast'
+
+
+
+#Use of the commands to use the API besithe its key
+parameters = {'q': 'Paris,FR',
+              'appid': 'b243bade4bbe1443aaa55ac385b0851d'}
+
+response = requests.get(url, params=parameters)
+
+
+
+#print(response.text)
+
+
+
